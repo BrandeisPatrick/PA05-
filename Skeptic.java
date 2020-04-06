b93d7b213e05f809a23fc7efcd06395c65c0dcf3
kyrarivest <57569515+kyrarivest@users.noreply.github.com>
2020-04-06 19:50:37 -0400

    Made the skeptic person class

---
+++ Skeptic.java
@@ -0,0 +1,15 @@
+
+public class Skeptic extends Person{
+
+	public Skeptic() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public void tryToMove() {
+		super.tryToMoveRandomly();
+	    
+	}
+
+
+}
