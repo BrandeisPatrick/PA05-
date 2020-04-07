0095cfa4155771084b229321d1f6be415ccb79cb
caelangm <62913521+caelangm@users.noreply.github.com>
2020-04-06 20:09:23 -0400

    made skeptic stuff there

--- MixedPopulation.java
+++ MixedPopulation.java
@@ -3,13 +3,13 @@ package lesson19;
 public class MixedPopulation extends Population{
   int numShelterInPlace;
   int numEssential;
-  int numOthers;
+  int numSkeptic;
 
-  public MixedPopulation(int numShelterInPlace, int numEssential, int numOthers){
-    super(numShelterInPlace + numEssential + numOthers);
+  public MixedPopulation(int numShelterInPlace, int numEssential, int numSkeptic){
+    super(numShelterInPlace + numEssential + numSkeptic);
     this.numShelterInPlace = numShelterInPlace;
     this.numEssential = numEssential;
-    this.numOthers = numOthers;
+    this.numSkeptic = numSkeptic;
 
   }
 
@@ -20,8 +20,8 @@ public class MixedPopulation extends Population{
     for(int i=0; i<this.numEssential; i++){
       this.addPerson(new StayAtHomeIfSick());
     }
-    for(int i=0; i<this.numOthers; i++){
-      this.addPerson(new Person());
+    for(int i=0; i<this.numSkeptic; i++){
+      this.addPerson(new Skeptic());
     }
   }
 }
