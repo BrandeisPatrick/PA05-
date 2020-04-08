1fcac1bfb7e17cdc910e70997076d06f095811fd
caelangm <62913521+caelangm@users.noreply.github.com>
2020-04-08 15:31:37 -0400

    added frequent flier

--- src/lesson19/MixedPopulation.java
+++ src/lesson19/MixedPopulation.java
@@ -4,12 +4,14 @@ public class MixedPopulation extends Population{
   int numShelterInPlace;
   int numEssential;
   int numSkeptic;
+  int numFreqFlier;
 
-  public MixedPopulation(int numShelterInPlace, int numEssential, int numSkeptic){
-    super(numShelterInPlace + numEssential + numSkeptic);
+  public MixedPopulation(int numShelterInPlace, int numEssential, int numSkeptic, int numFreqFlier){
+    super(numShelterInPlace + numEssential + numSkeptic + numFreqFlier);
     this.numShelterInPlace = numShelterInPlace;
     this.numEssential = numEssential;
     this.numSkeptic = numSkeptic;
+    this.numFreqFlier = numFreqFlier;
 
   }
 
@@ -23,5 +25,8 @@ public class MixedPopulation extends Population{
     for(int i=0; i<this.numSkeptic; i++){
       this.addPerson(new Skeptic());
     }
+    for(int i=0; i<this.numFreqFlier; i++){
+        this.addPerson(new FrequentFlier());
+      }
   }
 }
