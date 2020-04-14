e64670a679cd50c6acee136b6e6db430efd60c94
BrandeisPatrick <59629055+brandeispatrick@users.noreply.github.com>
2020-04-13 23:55:05 -0400

    - Count issue

--- src/lesson19/AnalyzeSimulation.java
+++ src/lesson19/AnalyzeSimulation.java
@@ -8,16 +8,17 @@ import java.util.Random;
 
 public class AnalyzeSimulation {
 
-	public static int days = 0;
-	public static int sick = 0;
-	public static int maxInfected = 0;
 	//Variables that will be printed
 
-	private static int MAX_TICKS=1000;
+	private static final int MAX_TICKS=1000;
 	private Random random = new Random();
 
 
 	public static void main(String[] args) {
+		int days = 0;
+		int sick = 0;
+		int maxInfected = 0;
+
 		int totalDays = 0;
 		int totalSick = 0;
 
@@ -62,8 +63,11 @@ public class AnalyzeSimulation {
 			}
 
 			sick += country.numInfected + country.numRecovered;
+			System.out.println("Days" + days );
+			System.out.println("sick" + country.numInfected );
+			System.out.println("reck" + country.numRecovered );
+			System.out.println();
 		}
-
 		//prints final stats
 		System.out.println("Average number of days until no new infections: " + (days/iterations));
 		System.out.println("Average number of people infected/recovered: " + (sick/iterations));
